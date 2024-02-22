9a46c0c9ff92dec6f75f3eed825717ba90adaabd
Ajay Rawat <arj69619@gmail.com>
2024-02-22 12:07:05 +0530

    version update

--- src/main/java/com/justdoeit/servlets/DeleteServlet.java
+++ src/main/java/com/justdoeit/servlets/DeleteServlet.java
@@ -2,16 +2,16 @@ package com.justdoeit.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.justdoeit.entities.JustDoeIt;
 import com.justdoeit.helper.FactoryProvider;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public class DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/justdoeit/servlets/SaveNoteServlet.java
+++ src/main/java/com/justdoeit/servlets/SaveNoteServlet.java
@@ -4,16 +4,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.justdoeit.entities.JustDoeIt;
 import com.justdoeit.helper.FactoryProvider;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public class SaveNoteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
